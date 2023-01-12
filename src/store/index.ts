69716b1843b8dc25f9ad5ab09a44a23d6642bef1
Matt Cosand <matt@cosand.net>
2023-01-12 10:18:28 -0800

    Update pre-reqs for Orientation/Course 3

--- src/store/index.ts
+++ src/store/index.ts
@@ -76,8 +76,8 @@ class Store implements AppChrome {
     { id: 'course-1', title: 'Course I', summary: "Outdoor weekend - Navigation", category: 'session', prereqs: ['course-c'], hours: 31, offerings: [] },
     { id: 'course-2', title: 'Course II', summary: "Outdoor weekend - Evaluation", category: 'session', prereqs: ['course-1'], hours: 31, offerings: [] },
     { id: 'fa-searcher', title: 'Searcher First Aid', summary: 'SAR specific first aid and scenarios', category: 'session', hours: 9, prereqs: ['course-2', 'fa-intro'], offerings: []},
-    { id: 'course-3', title: 'Course III', summary: "Outdoor weekend - mock mission", category: 'session', prereqs: ['fa-searcher', 'ics-100', 'ics-700'], hours: 31, offerings: [] },
-    { id: 'orientation', title: 'ESAR Ops Orientation', summary: 'Information for new graduates about responding to missions, etc.', category: 'session', prereqs: ['course-3'], hours: 3, offerings: [] }
+    { id: 'course-3', title: 'Course III', summary: "Outdoor weekend - mock mission", category: 'session', prereqs: ['course-2'], hours: 31, offerings: [] },
+    { id: 'orientation', title: 'ESAR Ops Orientation', summary: 'Information for new graduates about responding to missions, etc.', category: 'session', prereqs: ['course-2'], hours: 3, offerings: [] }
   ];
 
   constructor() {
