daec458692e35c5f34629bab4239f7e1f81ab2c3
Matt Cosand <matt@cosand.net>
2025-10-25 19:57:48 -0700

    Update pre-reqs (SFA vs Course 2)

--- src/store/index.ts
+++ src/store/index.ts
@@ -74,8 +74,8 @@ class Store implements AppChrome {
     //{ title: 'LFL Registration', summary: "For youth members", category: 'paperwork' },
 //      { title: 'Submit Photo', summary: "Submit portrait for ID card", category: 'paperwork' },
     { id: 'course-1', title: 'Course I', summary: "Outdoor weekend - Navigation", category: 'session', prereqs: ['course-c'], hours: 31, offerings: [] },
-    { id: 'course-2', title: 'Course II', summary: "Outdoor weekend - Evaluation", category: 'session', prereqs: ['course-1'], hours: 31, offerings: [] },
-    { id: 'fa-searcher', title: 'Searcher First Aid', summary: 'SAR specific first aid and scenarios', category: 'session', hours: 9, prereqs: ['course-2', 'fa-intro'], offerings: []},
+    { id: 'fa-searcher', title: 'Searcher First Aid', summary: 'SAR specific first aid and scenarios', category: 'session', hours: 9, prereqs: ['course-1','fa-intro'], offerings: []},
+    { id: 'course-2', title: 'Course II', summary: "Outdoor weekend - Evaluation", category: 'session', prereqs: ['course-1', 'fa-searcher'], hours: 31, offerings: [] },
     { id: 'course-3', title: 'Course III', summary: "Outdoor weekend - mock mission", category: 'session', prereqs: ['course-2'], hours: 31, offerings: [] },
     { id: 'orientation', title: 'ESAR Ops Orientation', summary: 'Information for new graduates about responding to missions, etc.', category: 'session', prereqs: ['course-2'], hours: 3, offerings: [] }
   ];
